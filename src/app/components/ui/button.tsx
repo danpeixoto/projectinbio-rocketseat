a28513f6792b880a1ad815cc77b60356eb41cff2
Daniel Peixoto <developer.peixoto@gmail.com>
2025-03-21 21:13:30 -0400

    feat: aula - continuação hero section

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import { cn } from "../../lib/utils";
 
 export default function Button({ children, variant = 'primary', ...props }:
-  { children: React.ReactNode, variant: "primary" | 'secondary' | 'ghost' } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  { children: React.ReactNode, variant?: "primary" | 'secondary' | 'ghost' } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
