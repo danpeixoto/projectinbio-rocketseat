886422aacaa337157699f8447435ac7b82e59476
Daniel Peixoto <developer.peixoto@gmail.com>
2025-03-23 20:39:12 -0400

    feat: finalizando modulo(tem algum erro na minha config)

--- src/app/components/landing-page/header.tsx
+++ src/app/components/landing-page/header.tsx
@@ -1,16 +1,22 @@
+import { manageAuth } from "@app/actions/manage-auth";
+import { auth } from "@app/lib/auth";
 import Button from "../ui/button";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
       <div className="flex items-center gap-4">
-        <img src="/logo.svg" alt="ProjectInBio logo" />
+        <img src="/logo.svg" alt="ProjectInBio Logo" />
         <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
       </div>
       <div className="flex items-center gap-4">
-        <Button>Minha Página</Button>
-        <Button>Sair</Button>
+        {session && <Button>Minha Página</Button>}
+        <form action={manageAuth}>
+          <Button>{session ? "Sair" : "Login"}</Button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -3,7 +3,12 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 import "server-only";
 
-const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY as string, "base64").toString("utf-8")
+// Certifcado
+
+const decodedKey = Buffer.from(
+  process.env.FIREBASE_PRIVATE_KEY!,
+  "base64"
+).toString("utf-8");
 
 export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
@@ -11,12 +16,14 @@ export const firebaseCert = cert({
   privateKey: decodedKey,
 });
 
+// Instancia do app
 if (!getApps().length) {
   initializeApp({
     credential: firebaseCert,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
 export const db = getFirestore();
-export const storage = getStorage().bucket();
+
+export const storage = getStorage().bucket(process.env.FIREBASE_STORAGE_BUCKET);
